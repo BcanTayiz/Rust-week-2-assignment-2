f58eb236ec5af42740175688b2809238659ae6a3
berkingurcan <berkingurcan@yandex.com>
2022-09-08 17:03:23 +0300

    Assignment template

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+// Import Crates
+
+fn main() {
+    // Read Input Data from txt file
+    todo!()
+
+    // Create vector of strings for leaves
+    
+
+    // Hash inputs and append to vector
+    
+
+    // Then Write an algorithm that calculates the ROOT
+}
+
+// You can use them or just remove
+// Helper function to create a next leaves level may help you :)
+fn create_next_level(current_level: Vec::<String>) -> Vec::<String> {
+    todo!();
+}
+
+
+// Helper function to hash one input may help you
+fn hash_one_input(a: &str) -> String {
+    todo!();
+}
+
+// Helper function to hash two input
+fn hash_two_input(a: &str, b: &str) -> String {
+    todo!();
+}
