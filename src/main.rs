56d1c10d30d7e340395a234db7e0c904c79e2a64
berkingurcan <berkingurcan@yandex.com>
2022-09-10 11:35:28 +0300

    add tests

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main(filename: String) -> String {
     // Return the root hash as a String
 }
 
-// You can use them or just remove
+// You can use templates below or just remove
 // Helper function to create a next leaves level may help you :)
 fn create_next_level(current_level: Vec::<String>) -> Vec::<String> {
     todo!();
@@ -26,4 +26,42 @@ fn create_next_level(current_level: Vec::<String>) -> Vec::<String> {
 // Helper function may help you to hash an input or You can write macro rules
 fn hash_input(a: &str) -> String {
     todo!();
+}
+
+
+
+
+// Pass all tests!
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn input_0() {
+        let result = main("input0.txt".to_string());
+        assert_eq!(result, "ff41418be11ed77612aeb83ee0bcf97a5853a4c291e23bd4d4cc6435fcfabdf9");
+    }
+
+    #[test]
+    fn input_1() {
+        let result = main("input1.txt".to_string());
+        assert_eq!(result, "98a77b2c3ff5f6c2aca697f60b2aa2a1a2733be36dbd35bae23d517c2ad5985e");
+    }
+
+    #[test]
+    fn input_2() {
+        let result = main("input2.txt".to_string());
+        assert_eq!(result, "3c0fb0638de91551eae4e9d984d72034aa0693be37b51737e6b81bc489866e5e");
+    }
+
+    #[test]
+    fn input_3() {
+        let result = main("input3.txt".to_string());
+        assert_eq!(result, "f03b1c9163babeb728ac011fe0c2c9c69700a2f8ddde211ec07d621cdb322cfe");
+    }
+
+    #[test]
+    fn input_4() {
+        let result = main("input4.txt".to_string());
+        assert_eq!(result, "f83e74742fda659dfc07615881af796abafc434f591aeb23b9f4366abe03c597");
+    }
 }
